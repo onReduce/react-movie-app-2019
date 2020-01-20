ad99e5f265719b1426872aa605067514b3ed6109
gylee <gylee@knowre.com>
2020-01-20 12:06:49 +0900

    Updates

---
+++ precache-manifest.c072834ea2a19b2173a3ebac479d1a70.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69428051c03ec7f474eb59e40253a4f4",
+    "url": "/react-movie-app-2019-1/index.html"
+  },
+  {
+    "revision": "a3e3fe2ea675d11638fa",
+    "url": "/react-movie-app-2019-1/static/js/2.17c2f699.chunk.js"
+  },
+  {
+    "revision": "3219ffeed4a395191d76",
+    "url": "/react-movie-app-2019-1/static/js/main.3c75d0a8.chunk.js"
+  },
+  {
+    "revision": "71fb4b8f7db0763b23bd",
+    "url": "/react-movie-app-2019-1/static/js/runtime~main.87d95dfa.js"
+  }
+]);
